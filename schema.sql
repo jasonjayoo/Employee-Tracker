4115957167cc88071938ac0097f110f507fa8106
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-03-08 12:09:33 -0800

    added department table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,10 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
+
+-- DEPARTMENT TABLE
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
